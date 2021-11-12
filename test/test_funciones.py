37e64e861d17ca98f72038cbd7374df005b74f97
AntoYepes <ayepesq24@gmail.com>
2021-11-12 15:21:23 -0500

    init

--- test/test_funciones.py
+++ test/test_funciones.py
@@ -7,5 +7,16 @@
 # from paquete import  funciones as fun
 
 # print(fun.Antonia())
+import paquete.funciones as fun
 
+print(fun.Antonia())
+def test_func():
+    assert fun.sumar(5, 4) == 9
 
+    assert print('hallo') == None
+
+# def div(a,b):
+#     return a / b
+
+# assert div(4, 2) == 2
+    
